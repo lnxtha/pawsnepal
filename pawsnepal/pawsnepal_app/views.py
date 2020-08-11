80f33f9953c5f03a2ee7b0e151f38f1ddf4a96e9
lnxtha <linusxrstha@gmail.com>
2020-08-11 23:59:33 +0545

    blog_detaiL_page and related_posts

--- pawsnepal/pawsnepal_app/views.py
+++ pawsnepal/pawsnepal_app/views.py
@@ -3,6 +3,9 @@ from django.views import View
 from django.views.generic import DetailView
 from .models import Blog
 from .models import ITEM_CATEGORIES, BLOG_CATEGORIES
+from django.db.models import Q
+
+import operator
 
 item_categories = dict(ITEM_CATEGORIES)
 blog_categories = dict(BLOG_CATEGORIES)
@@ -14,8 +17,6 @@ class HomeView(View):
     def get(self, request, *args, **kwargs):
         template_name = 'index.html'
         context = {'blogs': Blog.objects.all()[:4],  'blog_categories':blog_categories }
-        print(item_categories)
-        print('*'*20)
         return render(request, template_name, context)
 
 
@@ -23,9 +24,32 @@ class BlogDetailView(DetailView):
     template_name = 'blogdetail.html'
     model = Blog
 
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['item_categories'] = item_categories.values()
         context['blog_categories'] = blog_categories.values()
+
+        # print(self.kwargs['pk'])
+
+        # FOR RELATED POSTS START ########
+        related_posts = Blog.objects.raw(
+            'select distinct id as id, tags as tags from pawsnepal_app_blog where id = '+str(self.kwargs['pk']))
+        tags = related_posts[0].tags.split('#')
+        tags = [i.replace(' ','') for i in tags]
+        tags.remove('')
+        related_blog_posts = []
+        for i in tags:
+            [related_blog_posts.append(i.title) for i in Blog.objects.filter(tags__contains=i)]
+
+        post_dictionary = {}
+        for i in related_blog_posts:
+            post_dictionary[i] = related_blog_posts.count(i)
+
+        related_blog_posts = sorted(post_dictionary.keys(), key=lambda kv: kv[1], reverse=True)[:3]
+
+        context['related_posts'] = Blog.objects.filter(Q(title__in = related_blog_posts) & ~Q(id= self.kwargs['pk']))
         return context
+        # FOR RELATED POSTS START ########
+
 
